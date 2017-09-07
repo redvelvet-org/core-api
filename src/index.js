de097f390a08e1eae86d6ac308202a0319959d27
Anoop Varghese <anupvarghese@gmail.com>
2017-09-07 23:17:30 +1000

    update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,5 @@ const app = express();
 app.use(routes);
 
 app.listen(process.env.PORT, () => {
-  console.log(`started server on ${process.env.PORT}`)
+  console.log(`started server on ${process.env.PORT}`);
 });
-
