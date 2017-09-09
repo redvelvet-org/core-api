cfce38a038ad20c1c7d5384cc337ba8a396d4e2f
Anoop Varghese <anupvarghese@gmail.com>
2017-09-09 23:30:35 +1000

    fix migrations

---
+++ migrations/20170908202700.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('organizations', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: '',
+      },
+      phone: { type: Sequelize.STRING, allowNull: false, defaultValue: '' },
+      email: { type: Sequelize.STRING, allowNull: false },
+      settings: { type: Sequelize.JSONB, allowNull: false, defaultValue: {} },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      deleted_at: { type: Sequelize.DATE, allowNull: true },
+    }),
+  down: queryInterface => queryInterface.dropTable('organizations'),
+};
